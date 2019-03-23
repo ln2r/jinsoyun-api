9ec5d6c3e0822fa49215fc82331bc8ea55db31aa
ln2r <scorch_62@live.com>
2019-03-23 15:19:51 +0700

    cleanup

--- src/core.js
+++ src/core.js
@@ -34,6 +34,12 @@ module.exports = {
                     })                   
     },
 
+    /**
+     * setDataFormat
+     * used to change the fetched data '_id' field into 'id'
+     * @param data unformatted data
+     * @returns formatted data
+     */
     setDataFormat: function setFormat(data){
 
         let formatted = JSON.parse(JSON.stringify(data).split('"_id":').join('"id":'));
